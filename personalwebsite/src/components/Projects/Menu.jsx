d0bfa3965a50cfa5c55bb64b3725410f4ec7544b
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2023-06-09 21:54:56 +0100

    update projects

--- personalwebsite/src/components/Projects/Menu.jsx
+++ personalwebsite/src/components/Projects/Menu.jsx
@@ -1,5 +1,5 @@
 import projet1 from '../../assets/projet1.PNG'
-
+import projet2 from '../../assets/projet2.PNG'
 
 const Menu = [
     {
@@ -8,6 +8,13 @@ const Menu = [
       title: "Personal Website",
       category: "Web Development",
       url:'https://github.com/ElbazAhmed/Personal-Portfolio-Website',
+    },
+    {
+      id: 2,
+      image: projet2,
+      title: "Application-Web-Gestion-PFEs",
+      category: "Web Development",
+      url:'https://github.com/ElbazAhmed/Application-Web-Gestion-PFEs',
     },
 ]
 
